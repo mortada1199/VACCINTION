8c29607bf013ab3147eba4c190e6200f52513436
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-02 14:21:12 +0200

    add store and update to child

---
+++ app/Http/Services/Child/ChildServices.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Services\Child;
+
+use App\Http\Requests\User\LoginRequest;
+use App\Models\Child;
+
+class ChildServices
+{
+    public static function store($request)
+    {
+        $child = Child::create([
+            'name' => $request->name,
+            'date' => $request->date,
+            'gender' => $request->gender,
+           
+        ]);
+        return $child;
+    }
+
+
+public function updateChild($request,$id)
+{
+
+    $child  = Child::where('id', $id)->first();
+    if($child != null){
+        $child->update([
+            'name' => $request->name?? $child->name,
+            'date' => $request->password?? $child->date,
+            'gender' => $request->email?? $child->emagenderil,
+        ]);
+        return $child;
+    }
+}
+    
+}
