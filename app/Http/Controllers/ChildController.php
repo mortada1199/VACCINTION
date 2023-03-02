61fbb7451d30ad320eca39b6cc3d07847da51d9d
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-02 14:24:07 +0200

    child controller

--- app/Http/Controllers/ChildController.php
+++ app/Http/Controllers/ChildController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Child\CreateChildRequest;
+use App\Http\Requests\Child\UpdateChildRequest;
+use App\Http\Services\Child\ChildServices;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class ChildController extends Controller
 {
-    //
+    protected    ChildServices $childservices;
+    public function __construct(ChildServices $childservices)
+    {
+        $this->childservices = $childservices;
+    }
+    //this function use to crate user
+    public function store(CreateChildRequest $request)
+    {    
+        $child = $this->childservices->store($request);
+        if ($child) {
+            return response()->json([
+     "statusCode" => "000",'message' => 'created successfully','data' => $child  ], 200);
+        }
+        return response()->json([
+    "statusCode" => "264", 'success' => false, 'message' => "User Dosn't belong to any profile " ], 200);
+    }
+
+    
+
+    public function Update(UpdateChildRequest $request,$id)
+    {
+        $child = $this->childservices->updateChild($request,$id);
+        if ($child != null) {
+            return response()->json([
+     "statusCode" => "000",'message' => 'Update successfully','data' => $child  ], 200);
+        }
+        return response()->json([
+    "statusCode" => "264", 'success' => false, 'message' => "User Dosn't Update " ], 200);
+    }
 }
